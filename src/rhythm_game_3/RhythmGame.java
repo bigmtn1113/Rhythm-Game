11427d5b16fd3159d5fb24da18396127a2d4b433
20140339 <kva231@naver.com>
2020-05-04 03:26:52 +0900

    시작 화면에 음악 삽입

--- src/rhythm_game_3/RhythmGame.java
+++ src/rhythm_game_3/RhythmGame.java
@@ -6,36 +6,36 @@ import java.awt.Image;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
-public class RhythmGame extends JFrame{
-	
+public class RhythmGame extends JFrame {
+
 	private Image screenImage;
 	private Graphics screenGraphic;
 	
 	private Image introBackground;
-	
+
 	public RhythmGame() {
-		setTitle("Rhythm Game");	// 창 제목 설정
-		setSize(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);	 // 창 사이즈 조절
-		setResizable(false);	// 창 사이즈 조절 불가
-		setLocationRelativeTo(null);	// 창을 화면 정중앙에 위치
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	// 창을 닫으면 프로그램도 종료. 이를 안 하면 창을 닫아도 프로그램은 돌아감.
-		setVisible(true);	// 창을 화면에 출력
+		setTitle("Rhythm Game"); // 창 제목 설정
+		setSize(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT); // 창 사이즈 조절
+		setResizable(false); // 창 사이즈 조절 불가
+		setLocationRelativeTo(null); // 창을 화면 정중앙에 위치
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // 창을 닫으면 프로그램도 종료. 이를 안 하면 창을 닫아도 프로그램은 돌아감.
+		setVisible(true); // 창을 화면에 출력
+
+		introBackground = new ImageIcon(Main.class.getResource("../images/introBackground.jpg")).getImage();
 		
-		introBackground = new ImageIcon(Main.class.getResource("../images/introBackground.jpg")).getImage();	// 이미지 초기화
-		
-		Music music = new Music("introMusic.mp3", true);	// 곡 이름, 종료 전까지 무한 반복 = true
-		music.start();
+		Music introMusic = new Music("introMusic.mp3", true); // 곡 이름, 종료 전까지 무한 반복 = true
+		introMusic.start();
 	}
-	
-	public void paint(Graphics g) {	// 가장 먼저 화면을 표시해주는 메소드
+
+	public void paint(Graphics g) { // 가장 먼저 화면을 표시해주는 메소드
 		screenImage = createImage(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
 		screenGraphic = screenImage.getGraphics();
 		screenDraw(screenGraphic);
-		g.drawImage(screenImage, 0, 0, null);	// 0, 0 위치에 screenImage가 그려짐.
+		g.drawImage(screenImage, 0, 0, null); // 0, 0 위치에 screenImage가 그려짐.
 	}
-	
+
 	public void screenDraw(Graphics g) {
 		g.drawImage(introBackground, 0, 0, null);
-		this.repaint();	// 다시 paint를 호출해 매 순간마다 화면을 표시함.
+		this.repaint(); // 다시 paint를 호출해 매 순간마다 화면을 표시함.
 	}
 }
