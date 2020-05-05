b9b921a97ee8648824e0e517cbac3580b0b93df3
20140339 <kva231@naver.com>
2020-05-05 22:35:32 +0900

    난이도 선택 버튼 및 게임 화면으로 이동하는 기능 추가

---
+++ src/rhythm_game_8/Main.java
@@ -0,0 +1,12 @@
+package rhythm_game_8;
+
+public class Main {
+	
+	public static final int SCREEN_WIDTH = 1280;	// 창 가로 사이즈 설정
+	public static final int SCREEN_HEIGHT = 720;	// 창 세로 사이즈 설정
+
+	public static void main(String[] args) {
+		new RhythmGame();
+	}
+
+}
